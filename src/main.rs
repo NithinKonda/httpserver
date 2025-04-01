6c19c4ef1987b2c2af3183708f8ad91baa28accb
NithinKonda <nithinkonda142@gmail.com>
2025-04-01 17:02:45 +0530

    refactor TcpServer struct to use String type for host and implement constructor and start method

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ use std::net::{TcpListener, TcpStream};
 
 
 struct TcpServer{
-    host: string,
+    host: String,
     port: u16,
 }
+
+
+impl TcpServer{
+    fn new(host:String, port :u16)->Self {
+        TcpServer { host, port }
+    }
+
+    fn start(&self){
+        
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
