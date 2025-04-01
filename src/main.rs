dccd97f117f00290301d3f1ddde2b8ac6d2ee745
NithinKonda <nithinkonda142@gmail.com>
2025-04-01 22:16:57 +0530

    refactor TcpServer to TCPServer with generic RequestHandler and update main function to use it

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,56 @@
+// main.rs
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-
-
 trait RequestHandler {
-    fn handle_request(&self, data : &[u8]) -> Vec<u8>;
+    fn handle_request(&self, data: &[u8]) -> Vec<u8>;
 }
 
-struct TcpServer{
+struct TCPServer<T: RequestHandler> {
     host: String,
     port: u16,
     handler: T,
 }
 
-
-impl<T:RequestHandler> TcpServer<T>{
+impl<T: RequestHandler> TCPServer<T> {
     fn new(host: String, port: u16, handler: T) -> Self {
         TCPServer { host, port, handler }
     }
 
+    fn start(&self) -> std::io::Result<()> {
+        let address = format!("{}:{}", self.host, self.port);
+        
+        let listener = TcpListener::bind(&address)?;
+
+        
+        println!("Listening at {}", address);
+        
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    let peer_addr = stream.peer_addr()?;
+                    println!("Connected by {}", peer_addr);
+                    
+
+                    self.handle_client(stream)?;
+                }
+                Err(e) => {
+                    eprintln!("Connection failed: {}", e);
+                }
+            }
+        }
+        
+        Ok(())
+    }
     
+
 }
 
 
-fn main() {
-    let server = TcpServer::new("127.0.0.1".to_string(), 8888);
-    server.start();
+
+fn main() -> std::io::Result<()> {
+    let handler = EchoHandler;
+    let server = TCPServer::new("127.0.0.1".to_string(), 8888, handler);
+    server.start()
 }
